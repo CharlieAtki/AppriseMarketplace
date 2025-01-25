06e1abbe2bb15a497b096eec837a6eeeb1f80cee
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2025-01-25 17:37:31 +0000

    Bug fix - for local dev

--- backend/index.js
+++ backend/index.js
@@ -58,9 +58,17 @@ app.use(session({
   name: 'connect.sid', // Session cookie name
 }));
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL,
+]
+
 // Enhanced security headers middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`); // Replace with your frontend domain
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -68,7 +76,10 @@ app.use((req, res, next) => {
 });
 
 app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND_URL}`); // Replace with your frontend domain
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
