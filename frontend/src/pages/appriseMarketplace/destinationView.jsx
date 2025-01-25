d608e45dcd9eff68c2bd7dfbb4763ac4730dd9ec
CharlieAtki <164878103+CharlieAtki@users.noreply.github.com>
2025-01-25 12:26:33 +0000

    Dynamic destinationView creation. The users can now click on the destination panel in the marketplace. This will then redirect them to a template webpage which dynamically adjusts its content

--- frontend/src/pages/appriseMarketplace/destinationView.jsx
+++ frontend/src/pages/appriseMarketplace/destinationView.jsx
@@ -1,6 +1,6 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import {useEffect} from "react";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ArrowLeft } from "lucide-react";
 import MarketplaceNavigationBar from "../../components/appriseMarketplace/marketplaceNavigationBar";
 
 const DestinationView = () => {
